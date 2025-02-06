3d776b4a4507978b7e2ca088e67b2b99b25e6f1f
Pravar More <88570821+pravar-more@users.noreply.github.com>
2025-02-06 15:28:27 +0530

    Update provider.tf

--- role-policy-terraraform/provider.tf
+++ role-policy-terraraform/provider.tf
@@ -1,11 +1,16 @@
 provider "aws" {
   alias  = "master"
-  region = var.master_account_region
+  region = var.master_account_region  
+  access_key = ""
+  secret_key = ""
+
 }
 
 provider "aws" {
   alias  = "child"
   region = var.child_account_region
+  access_key = ""
+  secret_key = ""
   assume_role {
     role_arn = "arn:aws:iam::${var.child_account_id}:role/${var.role_name}"
   }
